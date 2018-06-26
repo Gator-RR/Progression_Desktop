2cd9fb51eed4a9721547043270c57495bbd0eb25
Tanner <212456204@logon.ds.ge.com>
2018-06-26 08:55:19 -0500

    Added set options, probably need to change to dict.

--- Progression/prog_main.py
+++ Progression/prog_main.py
@@ -4,18 +4,17 @@ from tkinter import ttk
 from tkinter import StringVar
 
 
-def read_json():
+def read_json(file_name):
     CWD = os.path.abspath('.')
-    input_file = os.path.join(CWD, 'Progression\\fws.json')
-    with open(input_file, 'r') as fid:  # TODO: Hardcoded Name
+    input_file = os.path.join(CWD, file_name)
+    with open(input_file, 'r') as fid:
         data = json.load(fid)
+    return data
 
-    activties = data[::]
 
+def write_json(data):
     with open('data.json', 'w') as outputfile:
         json.dump(data, outputfile)
-
-
 
 
 class App:
@@ -34,12 +33,22 @@ class App:
         self.box.grid(column=0, row=0)
 
 
+
 if __name__ == '__main__':
     '''
         Launch GUI for progresson application
     '''
+    file_name = 'Progression\\fws.json'
+    activities_set =  set()
 
-    read_json()
+    fws = read_json(file_name)
+
+    activities_set.add(day["activities"] for day in fws)
+    print("hello")
+    activities_set.__getattribute__
+
+
+        
 
     # root = Tk()
     # app = App(root)
