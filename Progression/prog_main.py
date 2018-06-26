939fe1ffa9a8ecb17b03df924ba068e25db4abd6
Tanner <212456204@logon.ds.ge.com>
2018-06-26 15:45:14 -0500

    Working filted listbox

--- Progression/prog_main.py
+++ Progression/prog_main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tkinter as tk
 from tkinter import ttk
 from tkinter import StringVar
 
@@ -51,17 +52,67 @@ def get_activities():
     return activities_dict
 
 
+def on_keyrelease(event):
+
+    # get text from entry
+    value = event.widget.get()
+    value = value.strip().lower()
+
+    # get data from activit_names
+    if value == '':
+        data = activity_names
+    else:
+        data = []
+        for item in activity_names:
+            if value in item.lower():
+                data.append(item)
+
+    # update data in listbox
+    listbox_update(data)
+
+
+def listbox_update(data):
+    # delete previous data
+    listbox.delete(0, 'end')
+
+    # sorting data
+    data = sorted(data, key=str.lower)
+
+    # put new data
+    for item in data:
+        listbox.insert('end', item)
+
+
+def on_select(event):
+    # display element selected on list
+    print('(event) previous:', event.widget.get('active'))
+    print('(event)  current:', event.widget.get(event.widget.curselection()))
+    print('---')
+
+
 if __name__ == '__main__':
     '''
         Launch GUI for progresson application
     '''
     activities = get_activities()
 
-    # TODO: get string containing new workout
+    activity_names = activities.keys()
 
-    # TODO: Print keys contain the substring
-    # [value for key, value in programs.items() if 'new york' in key.lower()]
-    # https://stackoverflow.com/questions/10484261/find-dictionary-items-whose-key-matches-a-substring
+    root = tk.Tk()
+
+    entry = tk.Entry(root)
+    entry.pack()
+    entry.bind('<KeyRelease>', on_keyrelease)
+
+    listbox = tk.Listbox(root)
+    listbox.pack()
+    #listbox.bind('<Double-Button-1>', on_select)
+    listbox.bind('<<ListboxSelect>>', on_select)
+    listbox_update(activity_names)
+
+    root.mainloop()
+
+    # https://stackoverflow.com/questions/47839813/python-tkinter-autocomplete-combobox-with-like-search
 
     print("Hello")
 
